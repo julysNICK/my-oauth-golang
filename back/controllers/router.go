eec3bfb0ed17b7ecf26fe7dd590547811aa61051
julysNICK <julystube@gmail.com>
2023-01-03 19:03:06 -0300

    add cors configuration

--- back/controllers/router.go
+++ back/controllers/router.go
@@ -1,7 +1,20 @@
 package controllers
 
+import "github.com/go-chi/cors"
+
 func (c ServerConfig) Get() {
+
+	cF := cors.New(
+		cors.Options{
+			AllowedOrigins:   []string{"https://*", "http://*"},
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			AllowCredentials: false,
+		})
+
+	c.Routes.Use(cF.Handler)
 	c.Routes.HandleFunc("/users", GetUsers).Methods("GET")
 	c.Routes.HandleFunc("/register", c.Register).Methods("POST")
 	c.Routes.HandleFunc("/login", c.Login).Methods("POST")
+
 }
--- back/go.mod
+++ back/go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.13.0 // indirect
